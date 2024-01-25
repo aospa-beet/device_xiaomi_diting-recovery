ae478cb618467dd42a5f927646cb806ee9c3d8bc
Sugakesshaa <sugakesshaa@pixelos.net>
2024-01-26 04:20:30 +0800

    diting: Load touch kmodules from vendor
    - thanks to @ pjgowtham for pointing this out
    
    Change-Id: I888a15a19a4fe958925ca96de1f85e4c16ba6c20

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -168,7 +168,7 @@ RECOVERY_LIBRARY_SOURCE_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so \
     $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@1.0.so \
     $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@2.0.so
-TW_LOAD_VENDOR_MODULES := "adsp_loader_dlkm.ko"
+TW_LOAD_VENDOR_MODULES := "adsp_loader_dlkm.ko nt36xxx-spi.ko fts_touch_spi.ko"
 TW_LOAD_VENDOR_MODULES_EXCLUDE_GKI := true
 
 # TWRP Debug Flags
